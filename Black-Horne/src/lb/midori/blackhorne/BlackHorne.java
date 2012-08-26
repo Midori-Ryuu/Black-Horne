fa0520a22c2ec71b6b4f68fc202496778c0d94a8
Midori Ryuu <Ryuu.Midori@gmail.com>
2012-08-26 22:48:49 +0300

    V0.0006
    -Added apollo entity framework along with example. Not yet integrated.
    -Tried Box2D dynamic and static bodies. Collosion detection not yet
    implemented or tested.

--- Black-Horne/src/lb/midori/blackhorne/BlackHorne.java
+++ Black-Horne/src/lb/midori/blackhorne/BlackHorne.java
@@ -13,14 +13,14 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.math.*;
 
 public class BlackHorne implements ApplicationListener {
 
 	private static final int VIRTUAL_WIDTH = 1920;
 	private static final int VIRTUAL_HEIGHT = 1080;
 	private static final float ASPECT_RATIO = (float) VIRTUAL_WIDTH / (float) VIRTUAL_HEIGHT;
-
+	private static final float bR = 30f;
+	private static final float rB = 1/30f;
 	private Camera camera;
 	private Rectangle viewport;
 	private Rectangle oldViewport;
@@ -28,36 +28,29 @@ public class BlackHorne implements ApplicationListener {
 
 	private Texture texture;
 	private Texture texture2;
-	private Sprite sprite;
+	private Texture texture3;
 
 	World world;
 	Body groundBody;
 	Body body;
+	Body body2;
 	PolygonShape groundBox;
 
 	@Override
 	public void create() {
-		// float w = Gdx.graphics.getWidth();
-		// float h = Gdx.graphics.getHeight();
-
-		// camera = new OrthographicCamera(1, h / w);
 		sb = new SpriteBatch();
 		camera = new OrthographicCamera(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
 		camera.position.set(VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, 0);
-		texture = new Texture(Gdx.files.internal("data/playerS1.png"));
+		//texture = new Texture(Gdx.files.internal("data/playerS1.png"));
+		texture = new Texture(Gdx.files.internal("data/playerWLx1.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		texture2 = new Texture(Gdx.files.internal("data/moon.jpg"));
+		//texture2 = new Texture(Gdx.files.internal("data/moon.jpg"));
+		texture2 = new Texture(Gdx.files.internal("data/moonx.jpg"));
 		texture2.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		viewport = new Rectangle(0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
-		// System.out.println("viewport: " + viewport);
-		// TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-
-		// sprite = new Sprite(region);
-		// sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		// sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
-		// sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);
+		texture3 = new Texture(Gdx.files.internal("data/rocks.png"));
+		texture3.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		initPhysics();
-
 	}
 
 	public void initPhysics() {
@@ -67,27 +60,42 @@ public class BlackHorne implements ApplicationListener {
 		world = new World(gravity, doSleep);
 
 		BodyDef groundBodyDef = new BodyDef();
-		groundBodyDef.position.set(110.0f, 110.0f);
+		groundBodyDef.position.set(200f*rB, 300f*rB);
 
 		groundBody = world.createBody(groundBodyDef);
 		groundBox = new PolygonShape();
 
-		groundBox.setAsBox(36.0f, 34.0f);
+		groundBox.setAsBox(texture3.getWidth()*rB/2, texture3.getHeight()*rB/2);
 		groundBody.createFixture(groundBox, 0.0f);
 		// -------------------
 		BodyDef bodyDef = new BodyDef();
 		bodyDef.type = BodyType.DynamicBody;
-		bodyDef.position.set(110.0f, 320.0f);
+		bodyDef.position.set(200*rB, 800*rB);
 
 		body = world.createBody(bodyDef);
 		PolygonShape dynamicBox = new PolygonShape();
-		dynamicBox.setAsBox(36.0f, 34.0f);
+		dynamicBox.setAsBox(texture.getWidth()*rB/2, texture.getHeight()*rB/2);
 
 		FixtureDef fixtureDef = new FixtureDef();
 		fixtureDef.shape = dynamicBox;
-		fixtureDef.density = 111.0f;
-		fixtureDef.friction = 0.3f;
+		fixtureDef.density = 1.0f;
+		fixtureDef.friction = 1.0f;
 		body.createFixture(fixtureDef);
+		//----------------------
+		BodyDef bodyDef2 = new BodyDef();
+		bodyDef2.type = BodyType.DynamicBody;
+		bodyDef2.position.set(200*rB, 1300*rB);
+
+		body2 = world.createBody(bodyDef2);
+		PolygonShape dynamicBox2 = new PolygonShape();
+		dynamicBox2.setAsBox(texture.getWidth()*rB/2, texture.getHeight()*rB/2);
+
+		FixtureDef fixtureDef2 = new FixtureDef();
+		fixtureDef2.shape = dynamicBox2;
+		fixtureDef2.density = 100.0f;
+		fixtureDef2.friction = 0.1f;
+		body2.createFixture(fixtureDef2);
+
 	}
 
 	@Override
@@ -107,15 +115,16 @@ public class BlackHorne implements ApplicationListener {
 
 		world.step(1.0f / 60.f, 6, 2);
 		Vector2 position = body.getPosition();
+		Vector2 position2 = body2.getPosition();
 		float angle = body.getAngle();
-		 System.out.printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+		//System.out.printf("%4.2f %4.2f %4.2f\n", position.x*bR, position.y*bR, angle);
 
 		sb.setProjectionMatrix(camera.combined);
 		sb.begin();
 		sb.draw(texture2, camera.position.x - texture2.getWidth() / 2, camera.position.y - texture2.getHeight() / 2);
-		sb.draw(texture, position.x - texture.getWidth() / 2,  position.y - texture.getHeight() / 2);
-	//	sb.draw(texture, position.x, position.y);
-		sb.draw(texture, groundBody.getPosition().x-texture.getWidth()/2, groundBody.getPosition().y-texture.getHeight()/2);
+		sb.draw(texture, position.x*bR - texture.getWidth() / 2, position.y*bR - texture.getHeight() / 2);
+		sb.draw(texture, position2.x*bR - texture.getWidth() / 2, position2.y*bR - texture.getHeight() / 2);
+		sb.draw(texture3, groundBody.getPosition().x*bR - texture3.getWidth() / 2, groundBody.getPosition().y*bR - texture3.getHeight() / 2);
 		sb.end();
 	}
 
